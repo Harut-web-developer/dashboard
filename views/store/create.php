65214918ab5892c0a88a047f364e6eaaace4f3be
MariamManukyan <mariammanukyan27012002@gmail.com>
2023-09-19 06:07:04 -0800

    added Store controller

---
+++ views/store/create.php
@@ -0,0 +1,22 @@
+<?php
+
+use yii\helpers\Html;
+
+/** @var yii\web\View $this */
+/** @var app\models\Store $model */
+
+$this->title = 'Create Store';
+$this->params['breadcrumbs']['Home'] ='/';
+$this->params['breadcrumbs']['Store'] = '/store/index';
+$this->params['breadcrumbs']['Create Store'] = '/creat/index';
+
+?>
+<div class="store-create">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
