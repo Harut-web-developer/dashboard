c7c8e61dd58e557758ac0127c8f222296d4a35e2
Harut-web-developer <hovsepyanh1994@gmail.com>
2023-10-03 18:06:54 +0400

    add all files to chart

--- controllers/ChartController.php
+++ controllers/ChartController.php
@@ -17,10 +17,6 @@ class ChartController extends  Controller{
         $categories = Category::find()->select('id,name')->asArray()->all();
 
         return $this->render('index',[
-//            'maxPrice' => $maxPrice,
-//            'maxCount' => $maxCount,
-//            'ordersCount' => $ordersCount,
-//            'overageOrdersProcent' => $overageOrdersProcent,
             'stores' => $stores,
             'categories' => $categories,
         ]);
@@ -28,14 +24,16 @@ class ChartController extends  Controller{
 
     public function actionGetData(){
         $post = $this->request->post();
+        $store = $post['store'];
+        $category = $post['category'];
         $start = $post['start'];
         $end = $post['end'];
-        if($post['store'] == 0){
+        if($post['store'] == 0 && $post['category'] == 0){
             $maxPrice = OrderItems::find()->select('MAX(round(order_items.price/order_items.quantity)) as maxPrice,
              MAX(orders.total_price) as price,product.name,product.img')
                 ->leftJoin('product','product.id = order_items.product_id')
                 ->leftJoin('orders', 'orders.id = order_items.order_id')
-                ->where(['between', 'orders.date', $start, $end])
+                ->where(['BETWEEN', 'orders.date', $start, $end])
                 ->asArray()
                 ->one();
             $maxCount = OrderItems::find()->select('MAX(order_items.quantity) as maxCount,
@@ -43,30 +41,248 @@ class ChartController extends  Controller{
                 ->leftJoin('product','product.id = order_items.product_id')
                 ->leftJoin('orders', 'orders.id = order_items.order_id')
                 ->leftJoin('target', 'orders.store_id = target.store_id')
-                ->where(['between', 'orders.date', $start, $end])
+                ->where(['BETWEEN', 'orders.date', $start, $end])
                 ->asArray()
                 ->one();
             $ordersTotalPrice = Orders::find()->select('SUM(total_price) as total')->asArray()->one();
             $ordersCount = Orders::find()->count();
             $averagePrice = intval($ordersTotalPrice['total'] / $ordersCount);
             $overageOrdersProcent = round(($averagePrice / $maxPrice['price']) * 100);
-            $titles = Orders::find()->select('id, DATE(date) as date_only,SUM(total_price) as totalPrice')
-                ->groupBy('date_only')
+            $total_data = OrderItems::find()->select('target.target_price,DATE(orders.date) as date_only,
+            SUM(order_items.price) as price,SUM(order_items.revenue) as revenue')
+                ->leftJoin('orders','orders.id = order_items.order_id')
+                ->leftJoin('target', 'target.date = DATE(orders.date)')
+                ->where(['BETWEEN', 'orders.date', $start, $end])
+                ->groupBy('target.date')
                 ->asArray()
                 ->all();
-            foreach ($titles as $key => $title){
-            var_dump($key);
+                $label = [];
+                $revenue = [];
+                $price = [];
+                $target_price = [];
+            for($i = 0; $i < count($total_data); $i++){
+                array_push($label,$total_data[$i]['date_only']);
+                array_push($revenue,intval($total_data[$i]['revenue']));
+                array_push($price,intval($total_data[$i]['price']));
+                array_push($target_price,intval($total_data[$i]['target_price']));
             }
-//            var_dump($titles);
             $response = [];
             $response['maxPrice'] = $maxPrice;
             $response['maxCount'] = $maxCount;
             $response['overageProcent'] = $overageOrdersProcent;
             $response['ordersCount'] = $ordersCount;
             $response['ordersTotalPrice'] = $ordersTotalPrice;
-//            $response['title'] = $title;
-
+            $response['label'] = $label;
+            $response['revenue'] = $revenue;
+            $response['price'] = $price;
+            $response['target_price'] = $target_price;
             return json_encode($response);
+        }else if($post['category'] === 'null'){
+                $maxPrice = OrderItems::find()->select('MAX(round(order_items.price/order_items.quantity)) as maxPrice,
+                MAX(orders.total_price) as price, product.name,product.img')
+                    ->leftJoin('orders', 'orders.id = order_items.order_id')
+                    ->leftJoin('product','product.id = order_items.product_id')
+                    ->where(['between', 'DATE(orders.date)', $start, $end])
+                    ->andWhere(['=', 'orders.store_id', $store])
+                    ->asArray()
+                    ->one();
+            $maxCount = OrderItems::find()->select('MAX(order_items.quantity) as maxCount,
+            SUM(order_items.revenue) as revenue, product.name,product.img,target.target_price')
+                ->leftJoin('orders', 'orders.id = order_items.order_id')
+                ->leftJoin('product','product.id = order_items.product_id')
+                ->leftJoin('target', 'orders.store_id = target.store_id')
+                ->where(['between', 'DATE(orders.date)', $start, $end])
+                ->andWhere(['=', 'orders.store_id', $store])
+                ->andWhere('target.date = DATE(`orders`.`date`)')
+                ->asArray()
+                ->one();
+            $ordersTotalPrice = Orders::find()->select('SUM(total_price) as total')
+                ->where(['between', 'DATE(date)', $start, $end])
+                ->andWhere( ['=', 'store_id',$store])
+                ->asArray()
+                ->one();
+            $ordersCount = Orders::find()->where(['between', 'DATE(date)', $start, $end])
+                ->andWhere( ['=', 'store_id',$store])
+                ->count();
+            $averagePrice = intval($ordersTotalPrice['total'] / $ordersCount);
+            $overageOrdersProcent = round(($averagePrice / $maxPrice['price']) * 100);
+            $total_data = OrderItems::find()->select('target.target_price,DATE(orders.date) as date_only,
+            SUM(order_items.price) as price,SUM(order_items.revenue) as revenue')
+                ->leftJoin('orders','orders.id = order_items.order_id')
+                ->leftJoin('target', 'target.date = DATE(orders.date)')
+                ->where(['between', 'DATE(orders.date)', $start, $end])
+                ->andWhere( ['=', 'orders.store_id',$store])
+                ->groupBy('target.date')
+                ->asArray()
+                ->all();
+            $label = [];
+            $revenue = [];
+            $price = [];
+            $target_price = [];
+            for($i = 0; $i < count($total_data); $i++){
+                array_push($label,$total_data[$i]['date_only']);
+                array_push($revenue,intval($total_data[$i]['revenue']));
+                array_push($price,intval($total_data[$i]['price']));
+                array_push($target_price,intval($total_data[$i]['target_price']));
+            }
+            $response = [];
+            $response['maxPrice'] = $maxPrice;
+            $response['maxCount'] = $maxCount;
+            $response['overageProcent'] = $overageOrdersProcent;
+            $response['ordersCount'] = $ordersCount;
+            $response['ordersTotalPrice'] = $ordersTotalPrice;
+            $response['label'] = $label;
+            $response['revenue'] = $revenue;
+            $response['price'] = $price;
+            $response['target_price'] = $target_price;
+            return json_encode($response);
+        }else if ($post['store'] === 'null'){
+            $maxPrice = OrderItems::find()->select('MAX(round(order_items.price/order_items.quantity)) as maxPrice,
+                MAX(orders.total_price) as price, product.name,product.img')
+                ->leftJoin('orders', 'orders.id = order_items.order_id')
+                ->leftJoin('product','product.id = order_items.product_id')
+                ->leftJoin('category', 'category.id = product.category_id')
+                ->where(['between', 'DATE(orders.date)', $start, $end])
+                ->andWhere(['=', 'category.id', $category])
+                ->asArray()
+                ->one();
+            $maxCount = OrderItems::find()->select('MAX(order_items.quantity) as maxCount,
+            SUM(order_items.revenue) as revenue, product.name,product.img,target.target_price')
+                ->leftJoin('orders', 'orders.id = order_items.order_id')
+                ->leftJoin('product','product.id = order_items.product_id')
+                ->leftJoin('category', 'category.id = product.category_id')
+                ->leftJoin('target', 'orders.store_id = target.store_id')
+                ->where(['between', 'DATE(orders.date)', $start, $end])
+                ->andWhere(['=', 'category.id', $category])
+                ->andWhere('target.date = DATE(`orders`.`date`)')
+                ->asArray()
+                ->one();
+            $ordersTotalPrice = OrderItems::find()->select('SUM(order_items.price) as total')
+                ->leftJoin('orders', 'orders.id = order_items.order_id')
+                ->leftJoin('product','product.id = order_items.product_id')
+                ->leftJoin('category', 'category.id = product.category_id')
+                ->where(['between', 'DATE(orders.date)', $start, $end])
+                ->andWhere( ['=', 'category.id',$category])
+                ->asArray()
+                ->one();
+            $ordersCount = OrderItems::find()
+                ->leftJoin('orders', 'orders.id = order_items.order_id')
+                ->leftJoin('product','product.id = order_items.product_id')
+                ->leftJoin('category', 'category.id = product.category_id')
+                ->where(['between', 'DATE(orders.date)', $start, $end])
+                ->andWhere( ['=', 'category.id',$category])
+                ->count();
+            $averagePrice = intval($ordersTotalPrice['total'] / $ordersCount);
+            $overageOrdersProcent = round(($averagePrice / $maxPrice['price']) * 100);
+            $total_data = OrderItems::find()->select('target.target_price,DATE(orders.date) as date_only,
+            SUM(order_items.price) as price,SUM(order_items.revenue) as revenue')
+                ->leftJoin('orders','orders.id = order_items.order_id')
+                ->leftJoin('target', 'target.date = DATE(orders.date)')
+                ->leftJoin('product','product.id = order_items.product_id')
+                ->leftJoin('category', 'category.id = product.category_id')
+                ->where(['between', 'DATE(orders.date)', $start, $end])
+                ->andWhere( ['=', 'category.id',$category])
+                ->groupBy('target.date')
+                ->asArray()
+                ->all();
+            $label = [];
+            $revenue = [];
+            $price = [];
+            $target_price = [];
+            for($i = 0; $i < count($total_data); $i++){
+                array_push($label,$total_data[$i]['date_only']);
+                array_push($revenue,intval($total_data[$i]['revenue']));
+                array_push($price,intval($total_data[$i]['price']));
+                array_push($target_price,intval($total_data[$i]['target_price']));
+            }
+            $response = [];
+            $response['maxPrice'] = $maxPrice;
+            $response['maxCount'] = $maxCount;
+            $response['overageProcent'] = $overageOrdersProcent;
+            $response['ordersCount'] = $ordersCount;
+            $response['ordersTotalPrice'] = $ordersTotalPrice;
+            $response['label'] = $label;
+            $response['revenue'] = $revenue;
+            $response['price'] = $price;
+            $response['target_price'] = $target_price;
+            return json_encode($response);
+            }else{
+            $maxPrice = OrderItems::find()->select('round(order_items.price/order_items.quantity) as maxPrice,
+            MAX(orders.total_price) as price, product.name,product.img')
+                ->leftJoin('orders', 'orders.id = order_items.order_id')
+                ->leftJoin('product','product.id = order_items.product_id')
+                ->leftJoin('category', 'category.id = product.category_id')
+                ->where(['between', 'DATE(orders.date)', $start, $end])
+                ->andWhere(['=', 'category.id', $category])
+                ->andWhere(['=', 'orders.store_id', $store])
+                ->asArray()
+                ->one();
+            $maxCount = OrderItems::find()->select('MAX(order_items.quantity) as maxCount,
+            SUM(order_items.revenue) as revenue, product.name,product.img,target.target_price')
+                ->leftJoin('orders', 'orders.id = order_items.order_id')
+                ->leftJoin('product','product.id = order_items.product_id')
+                ->leftJoin('category', 'category.id = product.category_id')
+                ->leftJoin('target', 'orders.store_id = target.store_id')
+                ->where(['between', 'DATE(orders.date)', $start, $end])
+                ->andWhere(['=', 'category.id', $category])
+                ->andWhere(['=', 'orders.store_id', $store])
+                ->andWhere('target.date = DATE(`orders`.`date`)')
+                ->asArray()
+                ->one();
+            $ordersTotalPrice = OrderItems::find()->select('SUM(order_items.price) as total')
+                ->leftJoin('orders', 'orders.id = order_items.order_id')
+                ->leftJoin('product','product.id = order_items.product_id')
+                ->leftJoin('category', 'category.id = product.category_id')
+                ->where(['between', 'DATE(orders.date)', $start, $end])
+                ->andWhere( ['=', 'category.id',$category])
+                ->andWhere( ['=', 'orders.store_id',$store])
+                ->asArray()
+                ->one();
+            $ordersCount = OrderItems::find()
+                ->leftJoin('orders', 'orders.id = order_items.order_id')
+                ->leftJoin('product','product.id = order_items.product_id')
+                ->leftJoin('category', 'category.id = product.category_id')
+                ->where(['between', 'DATE(orders.date)', $start, $end])
+                ->andWhere( ['=', 'category.id',$category])
+                ->andWhere( ['=', 'orders.store_id',$store])
+                ->count();
+            $averagePrice = intval($ordersTotalPrice['total'] / $ordersCount);
+            $overageOrdersProcent = round(($averagePrice / $maxPrice['price']) * 100);
+            $total_data = OrderItems::find()->select('target.target_price,DATE(orders.date) as date_only,
+            SUM(order_items.price) as price,SUM(order_items.revenue) as revenue')
+                ->leftJoin('orders','orders.id = order_items.order_id')
+                ->leftJoin('target', 'target.date = DATE(orders.date)')
+                ->leftJoin('product','product.id = order_items.product_id')
+                ->leftJoin('category', 'category.id = product.category_id')
+                ->where(['between', 'DATE(orders.date)', $start, $end])
+                ->andWhere( ['=', 'category.id',$category])
+                ->andWhere( ['=', 'orders.store_id',$store])
+                ->groupBy('target.date')
+                ->asArray()
+                ->all();
+            $label = [];
+            $revenue = [];
+            $price = [];
+            $target_price = [];
+            for($i = 0; $i < count($total_data); $i++){
+                array_push($label,$total_data[$i]['date_only']);
+                array_push($revenue,intval($total_data[$i]['revenue']));
+                array_push($price,intval($total_data[$i]['price']));
+                array_push($target_price,intval($total_data[$i]['target_price']));
+            }
+            $response = [];
+            $response['maxPrice'] = $maxPrice;
+            $response['maxCount'] = $maxCount;
+            $response['overageProcent'] = $overageOrdersProcent;
+            $response['ordersCount'] = $ordersCount;
+            $response['ordersTotalPrice'] = $ordersTotalPrice;
+            $response['label'] = $label;
+            $response['revenue'] = $revenue;
+            $response['price'] = $price;
+            $response['target_price'] = $target_price;
+            return json_encode($response);
+//                var_dump($total_data);
+
         }
 
 
--- web/js/custom.js
+++ web/js/custom.js
@@ -50,10 +50,12 @@ $(document).ready(function (){
 
             workChart(0,0,start,end);
         })
-
+    
     function  workChart(store,category,start,end){
         var title = [];
-        var datatable1 = [];
+        var revenue = [];
+        var target_price = [];
+        var sellsPrice = [];
         var datatable2 = [];
         $.ajax({
             url:'/chart/get-data',
@@ -80,11 +82,16 @@ $(document).ready(function (){
             datatable2[0] = parseInt(parse.ordersTotalPrice.total);
             datatable2[1] = parseInt(parse.maxCount.revenue);
             datatable2[2] = parse.maxCount.target_price;
-            // console.log(datatable);
+            title = parse.label;
+            revenue = parse.revenue;
+            sellsPrice = parse.price;
+            target_price = parse.target_price;
+            // console.log(revenue);
+            // console.log(parse.revenue);
             })
+
         // Pie Chart Example
-        var ctx = document.getElementById("myPieChart");
-        var myPieChart = new Chart(ctx, {
+        var option = {
             type: 'doughnut',
             data: {
                 labels: ['sells','revenue','target revenue'],
@@ -112,92 +119,99 @@ $(document).ready(function (){
                 },
                 cutoutPercentage: 80,
             },
-        });
+        }
+        // myPieChart.update();
+        // Chart.exec('myPieChart', 'updateOptions', option_)
+
+        var myPieChart = new Chart(document.getElementById("myPieChart"), option);
+
+
+        var options = {
+            series: [{
+                name: "Revenue",
+                data: revenue
+            },
+                {
+                    name: "Target price",
+                    data: target_price
+
+                },
+                {
+                    name: 'Sells price',
+                    data: sellsPrice
+                }
+            ],
+            chart: {
+                height: 350,
+                type: 'line',
+                zoom: {
+                    enabled: false
+                },
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                width: [5, 7, 5],
+                curve: 'straight',
+                dashArray: [0, 8, 5]
+            },
+            title: {
+                text: 'Page Statistics',
+                align: 'left'
+            },
+            legend: {
+                tooltipHoverFormatter: function(val, opts) {
+                    return val + ' - ' + opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] + ''
+                }
+            },
+            markers: {
+                size: 0,
+                hover: {
+                    sizeOffset: 6
+                }
+            },
+            xaxis: {
+                categories: title,
+            },
+            tooltip: {
+                y: [
+                    {
+                        title: {
+                            formatter: function (val) {
+                                return val + " (mins)"
+                            }
+                        }
+                    },
+                    {
+                        title: {
+                            formatter: function (val) {
+                                return val + " per session"
+                            }
+                        }
+                    },
+                    {
+                        title: {
+                            formatter: function (val) {
+                                return val;
+                            }
+                        }
+                    }
+                ]
+            },
+            grid: {
+                borderColor: '#f1f1f1',
+            }
+        };
+
+        var chart = new ApexCharts(document.querySelector("#chart"), options);
+        chart.render();
+
+
+
     }
 
 
 
-    var options = {
-        series: [{
-            name: "Session Duration",
-            data: [45, 52, 38, 24, 33, 26, 21, 20, 6, 8, 15, 10]
-        },
-            {
-                name: "Page Views",
-                data: [35, 41, 62, 42, 13, 18, 29, 37, 36, 51, 32, 35]
-
-            },
-            {
-                name: 'Total Visits',
-                data: [87, 57, 74, 99, 75, 38, 62, 47, 82, 56, 45, 47]
-            }
-        ],
-        chart: {
-            height: 350,
-            type: 'line',
-            zoom: {
-                enabled: false
-            },
-        },
-        dataLabels: {
-            enabled: false
-        },
-        stroke: {
-            width: [5, 7, 5],
-            curve: 'straight',
-            dashArray: [0, 8, 5]
-        },
-        title: {
-            text: 'Page Statistics',
-            align: 'left'
-        },
-        legend: {
-            tooltipHoverFormatter: function(val, opts) {
-                return val + ' - ' + opts.w.globals.series[opts.seriesIndex][opts.dataPointIndex] + ''
-            }
-        },
-        markers: {
-            size: 0,
-            hover: {
-                sizeOffset: 6
-            }
-        },
-        xaxis: {
-            categories: ['01 Jan', '02 Jan', '03 Jan', '04 Jan', '05 Jan', '06 Jan', '07 Jan', '08 Jan', '09 Jan',
-                '10 Jan', '11 Jan', '12 Jan'
-            ],
-        },
-        tooltip: {
-            y: [
-                {
-                    title: {
-                        formatter: function (val) {
-                            return val + " (mins)"
-                        }
-                    }
-                },
-                {
-                    title: {
-                        formatter: function (val) {
-                            return val + " per session"
-                        }
-                    }
-                },
-                {
-                    title: {
-                        formatter: function (val) {
-                            return val;
-                        }
-                    }
-                }
-            ]
-        },
-        grid: {
-            borderColor: '#f1f1f1',
-        }
-    };
-
-    var chart = new ApexCharts(document.querySelector("#chart"), options);
-    chart.render();
 
 })
